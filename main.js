9193e111d0600505d747124f54c8e2ce77655918
Jenny_Ripper <jennifer.ripper@gmail.com>
2019-12-02 11:04:13 -0500

    square event click listener

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("testing")
+//create a variable for the grid-container id
+const gridContainer = document.getElementById("grid-container")
+//grab the nodelist within the grid-container for each grid-item on the board
+const gridItemList = gridContainer.querySelectorAll(".grid-item")
+//grab the color background within grid-item
+const gridItemColorList = gridContainer.querySelectorAll(".background-color")
+
+
+gridItemColorList.forEach((gridItem => {
+
+    gridItem.addEventListener("click", evt => {
+        evt.preventDefault();
+        gridItem.style.backgroundColor = "lightblue";
+    })
+    //console.log(gridItem)
+}))
