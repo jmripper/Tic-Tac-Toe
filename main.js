fdabc42573f64f5a9b125c873b910c7768aba01e
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-02-28 12:15:24 -0500

    finalized things

--- main.js
+++ main.js
@@ -59,19 +59,23 @@
 // }
 
 /*----- variables -----*/
+const player = "O";
+const computer = "X";
 
 let board;
-let user = "X"
-let ai = "0"
+let gameActive = true;
 let turn = "X";
 let win;
+let board_full = false;
+let play_board = ["", "", "", "", "", "", "", "", ""];
 
-const squares = Array.from(document.querySelectorAll("#board li"));
-const elementBoard = document.querySelector("#board");
+const squares = Array.from(document.querySelectorAll("#board div"));
+const gameBoard = document.querySelector("#board");
 const resetButton = document.getElementById("resetButton");
-const h3Text = document.querySelector("h3");
+const statusDisplay = document.querySelector(".game-status");
 const cell = document.querySelectorAll(".square");
-const winningCombos = [
+const winner_statement = document.querySelector(".game-status");
+const winningConditions = [
   [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
@@ -82,52 +86,15 @@ const winningCombos = [
   [2, 4, 6]
 ];
 
+document.querySelector("#board").addEventListener("click", handleTurn);
+document.querySelector(".reset-btn").addEventListener("click", game);
+
 /*----- functions -----*/
-
-function game() {
-  board = ["", "", "", "", "", "", "", "", ""];
-
-  render();
-}
-
-game();
-
-function render() {
-  board.forEach((mark, i) => {
-    squares[i].textContent = mark;
-  });
-
-  h3Text.innerHTML =
-    win === "T"
-      ? `Dang that's a tie.`
-      : win
-      ? `${win} wins the game!`
-      : `It's ${turn}'s turn.`;
-}
-
-elementBoard.addEventListener("click", evt => {
-  //console.log("square clicked")
-  let squareID = squares.findIndex(square => {
-    return square === evt.target;
-  });
-
-  board[squareID] = turn;
-
-  win = getWinner();
-
-  if (turn === "X") {
-    turn = "O";
-  } else {
-    turn = "X";
-  }
-
-  render();
-});
 
 function getWinner() {
   let winner = null;
 
-  winningCombos.forEach((combo, i) => {
+  winningConditions.forEach((combo, i) => {
     if (
       board[combo[0]] &&
       board[combo[0]] === board[combo[1]] &&
@@ -141,6 +108,143 @@ function getWinner() {
   return winner ? winner : board.includes("") ? null : "T";
 }
 
-resetButton.addEventListener("click", evt => {
-  game();
-});
+function handleTurn() {
+    let idx = squares.findIndex(square => {
+        return square === event.target;
+    })
+    board[idx] = turn;
+    turn = turn === "X" ? "O" : "X";
+    win = getWinner();
+    render();
+};
+
+function game () {
+  board = ["", "", "", "", "", "", "", "", ""];
+  render();
+};
+
+function render() {
+  board.forEach((mark, i) => {
+    squares[i].textContent = mark;
+  });
+
+  statusDisplay.textContent =
+    win === "T"
+      ? `That's a tie, play again?`
+      : win
+      ? `${win} wins the game!`
+      : `It's ${turn}'s turn!`;
+}
+
+game();
+
+// const render_board = () => {
+//   gameBoard.innerHTML = "";
+//   play_board.forEach((e, i) => {
+//     gameBoard.innerHTML += `<div id="${i}" class="square" onclick="addPlayerMove(${i})">${play_board[i]}</div>`;
+//     if (e == player || e == computer) {
+//       document.querySelector(`#${i}`).classList.add("occupied");
+//     }
+//   });
+// };
+
+// render_board();
+
+// const addPlayerMove = e => {
+//   if (play_board[e] == "") {
+//     play_board[e] = player;
+//     render_board();
+//     addComputerMove();
+//   }
+// };
+
+// const addPlayerMove = e => {
+//   if (!board_full && play_board[e] == "") {
+//     play_board[e] = player;
+//     game_loop();
+//     addComputerMove();
+//   }
+// };
+
+// const addComputerMove = () => {
+//   do {
+//     selected = Math.floor(Math.random() * 9);
+//   } while (play_board[selected] != "");
+//   play_board[selected] = computer;
+//   render_board();
+// };
+
+// const check_board_complete = () => {
+//   let flag = true;
+//   play_board.forEach(element => {
+//     if (element != player && element != computer) {
+//       flag = false;
+//     }
+//   });
+//   board_full = !flag;
+// };
+
+// const game_loop = () => {
+//   render_board();
+//   check_board_complete();
+// };
+
+// const check_for_winner = () => {
+//   let res = check_match();
+//   if (res == player) {
+//     winner.innerHTML = "Winner is player!!";
+//     winner.classList.add("playerWin");
+//     board_full = true;
+//   } else if (res == computer) {
+//     winner.innerText = "Winner is computer";
+//     winner.classList.add("computerWin");
+//     board_full = true;
+//   } else if (board_full) {
+//     winner.innerText = "Draw!";
+//     winner.classList.add("draw");
+//   }
+// };
+
+// const check_line = (a, b, c) => {
+//   return (
+//     play_board[a] == play_board[b] &&
+//     play_board[b] == play_board[c] &&
+//     (play_board[a] == player || play_board[a] == computer)
+//   );
+// };
+
+// const check_match = () => {
+//   for (i = 0; i < 9; i += 3) {
+//     if (check_line(i, i + 1, i + 2)) {
+//       return play_board[i];
+//     }
+//   }
+//   for (i = 0; i < 3; i++) {
+//     if (check_line(i, i + 3, i + 6)) {
+//       return play_board[i];
+//     }
+//   }
+//   if (check_line(0, 4, 8)) {
+//     return play_board[0];
+//   }
+//   if (check_line(2, 4, 6)) {
+//     return play_board[2];
+//   }
+//   return "";
+// };
+
+// const game_loop = () => {
+//   render_board();
+//   check_board_complete();
+//   check_for_winner();
+// };
+
+// const reset_board = () => {
+//     play_board = ["", "", "", "", "", "", "", "", ""];
+//     board_full = false;
+//     winner.classList.remove("playerWin");
+//     winner.classList.remove("computerWin");
+//     winner.classList.remove("draw");
+//     winner.innerText = "";
+//     render_board();
+// }
